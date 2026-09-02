481c8cac2c7cda2e19ff0000627a6eebe52656cd
FellypeGC <gabrielfellypesteve@gmail.com>
2026-09-01 23:59:42 -0300

    chore: understand app router - dynamic routes

---
+++ src/app/about/layout.tsx
@@ -0,0 +1,10 @@
+export default function AboutPage({ children }: {
+  children: React.ReactNode
+}) {
+  return (
+    <>
+      <h1>Here's the layout for the about page</h1>
+      {children}
+    </>
+  );
+}
---
+++ src/app/about/me/page.tsx
@@ -0,0 +1,10 @@
+export default function AboutMePage({ children }: {
+  children: React.ReactNode
+}) {
+  return (
+    <>
+      <h1>Me</h1>
+      {children}
+    </>
+  );
+}

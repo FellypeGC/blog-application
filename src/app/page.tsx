17213a75316df0caff37b7942c18c78beb974794
FellypeGC <gabrielfellypesteve@gmail.com>
2026-09-11 19:42:11 -0300

    feat: add featuredpost component & tailwind css grid

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,8 @@ import { PostsList } from '@/components/PostsList';
 import { SpinLoader } from '@/components/SpinLoader';
 
 import { Suspense } from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
 
 // page.tsx (server) -> menu.tsx (server) -> link (client)
 
@@ -11,6 +13,24 @@ export default async function HomePage() {
   return (
     <Container>
       <Header />
+
+      <section className='grid grid-cols-1 gap-8 mb-16 sm:grid-cols-2 group'>
+        <Link className='w-full h-full overflow-hidden rounded-xl' href='#'>
+          <Image
+            className='group-hover:scale-105 transition duration-300'
+            src='/images/bryen_0.png'
+            width={1200}
+            height={720}
+            alt='Título do post'
+          />
+        </Link>
+        <div>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi
+          quibusdam dolorum, exercitationem numquam, maxime doloribus nisi
+          consequatur, repellendus officiis dolores laborum. Consequatur aliquam
+          eum fugiat, modi debitis at nostrum sit.
+        </div>
+      </section>
 
       <Suspense fallback={<SpinLoader />}>
         <PostsList />

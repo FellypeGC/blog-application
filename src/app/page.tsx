a07c08e3a89e0b27d822b80699a0561575ec1ca8
FellypeGC <gabrielfellypesteve@gmail.com>
2026-09-11 20:06:53 -0300

    chore: finish featured post css with tailwind css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,18 +17,32 @@ export default async function HomePage() {
       <section className='grid grid-cols-1 gap-8 mb-16 sm:grid-cols-2 group'>
         <Link className='w-full h-full overflow-hidden rounded-xl' href='#'>
           <Image
-            className='group-hover:scale-105 transition duration-300'
+            className='w-full h-full object-cover object-center group-hover:scale-105 transition duration-300'
             src='/images/bryen_0.png'
             width={1200}
             height={720}
             alt='Título do post'
+            priority
           />
         </Link>
-        <div>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi
-          quibusdam dolorum, exercitationem numquam, maxime doloribus nisi
-          consequatur, repellendus officiis dolores laborum. Consequatur aliquam
-          eum fugiat, modi debitis at nostrum sit.
+
+        <div className='flex flex-col gap-4 sm:justify-center'>
+          <time className='text-slate-600 mb-4 block text-sm/tight' dateTime='2025-04-20'>
+            20/04/2025 10:00
+          </time>
+
+          <h1 className='text-2xl/tight mb-4 font-extrabold sm:text-4xl'>
+            <Link href='#'>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            </Link>
+          </h1>
+
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi
+            quibusdam dolorum, exercitationem numquam, maxime doloribus nisi
+            consequatur, repellendus officiis dolores laborum. Consequatur
+            aliquam eum fugiat, modi debitis at nostrum sit.
+          </p>
         </div>
       </section>
 

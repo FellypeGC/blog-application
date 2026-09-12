dddb7cbf714f1a9595580eb1d30deedf5dd88588
FellypeGC <gabrielfellypesteve@gmail.com>
2026-09-11 22:21:31 -0300

    chore: PostImageCover component with Link & Image from Next

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,8 @@ import { PostsList } from '@/components/PostsList';
 import { SpinLoader } from '@/components/SpinLoader';
 
 import { Suspense } from 'react';
-import Link from 'next/link';
-import Image from 'next/image';
 import { PostHeading } from '@/components/PostHeading';
+import { PostCoverImage } from '@/components/PostCoverImage';
 
 // page.tsx (server) -> menu.tsx (server) -> link (client)
 
@@ -16,19 +15,24 @@ export default async function HomePage() {
       <Header />
 
       <section className='grid grid-cols-1 gap-8 mb-16 sm:grid-cols-2 group'>
-        <Link className='w-full h-full overflow-hidden rounded-xl' href='#'>
-          <Image
-            className='w-full h-full object-cover object-center group-hover:scale-105 transition duration-300'
-            src='/images/bryen_0.png'
-            width={1200}
-            height={720}
-            alt='Título do post'
-            priority
-          />
-        </Link>
+        <PostCoverImage
+          linkProps={{
+            href: '/post/afwag',
+          }}
+          imageProps={{
+            width: 1200,
+            height: 720,
+            src: '/images/bryen_9.png',
+            alt: 'alt da imagem',
+            priority: true,
+          }}
+        />
 
         <div className='flex flex-col gap-4 sm:justify-center'>
-          <time className='text-slate-600 mb-4 block text-sm/tight' dateTime='2025-04-20'>
+          <time
+            className='text-slate-600 mb-4 block text-sm/tight'
+            dateTime='2025-04-20'
+          >
             20/04/2025 10:00
           </time>
 

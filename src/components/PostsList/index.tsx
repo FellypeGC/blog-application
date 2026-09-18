a227dec789c40ceca7c67b5b4e5f91b6488e9116
FellypeGC <gabrielfellypesteve@gmail.com>
2026-09-18 00:11:06 -0300

    refactor(feat): display formatted date in the components

--- src/components/PostsList/index.tsx
+++ src/components/PostsList/index.tsx
@@ -1,6 +1,7 @@
 import { postRepository } from '@/repositories/post';
 import { PostCoverImage } from '../PostCoverImage';
 import { PostHeading } from '../PostHeading';
+import { formatDatetime, formatDistanceToNow } from '@/utils/format-datetime';
 
 export async function PostsList() {
   const posts = await postRepository.findAll();
@@ -28,17 +29,16 @@ export async function PostsList() {
               <time
                 className='text-slate-600 mb-4 block text-sm/tight'
                 dateTime={post.createdAt}
+                title={formatDistanceToNow(post.createdAt)}
               >
-                {post.createdAt}
+                {formatDatetime(post.createdAt)}
               </time>
 
               <PostHeading as='h2' url={postLink}>
                 {post.title}
               </PostHeading>
 
-              <p>
-                {post.excerpt}
-              </p>
+              <p>{post.excerpt}</p>
             </div>
           </div>
         );

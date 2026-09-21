3a05e1ae0a309ecaa14202fe4a26b83595c52172
FellypeGC <gabrielfellypesteve@gmail.com>
2026-09-21 19:02:56 -0300

    refactor: add react cache to remove duplication of API/Repository calls

--- src/components/PostFeatured/index.tsx
+++ src/components/PostFeatured/index.tsx
@@ -1,10 +1,11 @@
 import { PostCoverImage } from '../PostCoverImage';
-import { PostHeading } from '../PostHeading';
 import { PostSummary } from '../PostSummary';
+import { findAllPublicPosts } from '@/lib/post/queries';
 
-export function PostFeatured() {
-  const slug = 'anything';
-  const postLink = `/post/${slug}`;
+export async function PostFeatured() {
+  const posts = await findAllPublicPosts();
+  const post = posts[0];
+  const postLink = `/post/${post.slug}`;
 
   return (
     <section className='grid grid-cols-1 gap-8 mb-16 sm:grid-cols-2 group'>
@@ -15,8 +16,8 @@ export function PostFeatured() {
         imageProps={{
           width: 1200,
           height: 720,
-          src: '/images/bryen_9.png',
-          alt: 'alt da imagem',
+          src: post.coverImageUrl,
+          alt: post.title,
           priority: true,
         }}
       />
@@ -24,9 +25,9 @@ export function PostFeatured() {
       <PostSummary
         postLink={postLink}
         postHeading='h1'
-        createdAt={'2025-04-08T00:33:56.907Z'}
-        title={'Rotina matinal de pessoas altamente eficazes'}
-        excerpt={'O Next.js também é uma boa escolha para quem quer se preocupar com performance e SEO.'}
+        createdAt={post.createdAt}
+        title={post.title}
+        excerpt={post.excerpt}
       />
     </section>
   );

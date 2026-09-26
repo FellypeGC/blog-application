3fd96988c37432c91571f3d9ae040842961e2a9f
FellypeGC <gabrielfellypesteve@gmail.com>
2026-09-25 21:29:44 -0300

    feat: throw notFound & create finndBySlug in repository

--- src/repositories/post/json-post-repository.ts
+++ src/repositories/post/json-post-repository.ts
@@ -43,4 +43,13 @@ export class JsonPostRepository implements PostRepository {
 
     return post;
   }
+
+  async findBySlug(slug: string): Promise<PostModel | undefined> {
+    const posts = await this.findAllPublic();
+    const post = posts.find(post => post.slug === slug);
+
+    if (!post) throw new Error(`Post with slug ${slug} not found`);
+
+    return post;
+  }
 }
